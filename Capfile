1899eae76353d8ccc442d676bc9c83bca686364b
jun0222 <nextseason6@gmail.com>
2019-03-05 06:41:38 +0900

    fix Capistrano

--- Capfile
+++ Capfile
@@ -1,8 +1,8 @@
 # Load DSL and set up stages
-# require "capistrano/setup"
+require "capistrano/setup"
 
 # Include default deployment tasks
-# require "capistrano/deploy"
+require "capistrano/deploy"
 
 # Load the SCM plugin appropriate to your project:
 #
@@ -20,29 +20,21 @@
 # For documentation on these, see for example:
 #
 #   https://github.com/capistrano/rvm
-#   https://github.com/capistrano/rbenv
+  # https://github.com/capistrano/rbenv
 #   https://github.com/capistrano/chruby
 #   https://github.com/capistrano/bundler
 #   https://github.com/capistrano/rails
 #   https://github.com/capistrano/passenger
-#
+
+# set :rbenv_type, :system
+# set :rbenv_ruby, '2.3.1'
 # require "capistrano/rvm"
-# require "capistrano/rbenv"
+require "capistrano/rbenv"
 # require "capistrano/chruby"
-# require "capistrano/bundler"
-# require "capistrano/rails/assets"
-# require "capistrano/rails/migrations"
+require "capistrano/bundler"
+require "capistrano/rails/assets"
+require "capistrano/rails/migrations"
 # require "capistrano/passenger"
-
-# Load custom tasks from `lib/capistrano/tasks` if you have any defined
-
-require "capistrano/setup"
-require "capistrano/deploy"
-
-require 'capistrano/rbenv'
-require 'capistrano/bundler'
-require 'capistrano/rails/assets'
-require 'capistrano/rails/migrations'
 require 'capistrano3/unicorn'
-
+# Load custom tasks from `lib/capistrano/tasks` if you have any defined
 Dir.glob("lib/capistrano/tasks/*.rake").each { |r| import r }
--- config/deploy.rb
+++ config/deploy.rb
@@ -1,15 +1,27 @@
-# config valid for current version and patch releases of Capistrano
-
-
-set :application, "chat-space2"
-set :repo_url, "git@example.com:jun0222/chat-space2.git"
-
-set :stages, ["production"]
+lock '~> 3.11.0'
+#アプリケーション名
+set :application,'chat-space2'
+#レポジトリURL
+set :repo_url, 'git@github.com:jun0222/chat-space2.git'
+#対象ブランチ masterに固定
+# set :branch, 'master'
+#デプロイ先ディレクトリ フルパスで指定
+# set :deploy_to, '/var/www/test'
+#バージョン管理方法 subverion, git, mercurial, cvs, bzrなど
+# set :scm, :git
+#情報レベル info or debug
+# set :log_level, :debug
+#sudoに必要 これをtrueにするとssh -tで実行される
+# set :pty, true
+#sharedに入るものを指定
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
-
+#capistrano用bundleするのに必要
 set :rbenv_type, :user
 set :rbenv_ruby, '2.3.1'
 
+# set :default_env, { path: "/usr/local/rbenv/shims:/usr/local/rbenv/bin:$PATH" }
+#5回分のreleasesを保持する
+# set :keep_releases, 5
 set :ssh_options, auth_methods: ['publickey'],
                   keys: ['~/.ssh/pengin.pem']
 
@@ -18,11 +30,31 @@ set :unicorn_config_path, -> { "#{current_path}/config/unicorn.rb" }
 set :keep_releases, 5
 
 after 'deploy:publishing', 'deploy:restart'
-namespace :deploy do
-  task :restart do
-    invoke 'unicorn:restart'
-  end
+
+#タスク定義
+namespace :deploy do #タスクnamespace
+    #desc 'タスク説明'
+    #task :restart do #タスク定義
+        #ここにタスク処理内容を記述
+    #end
+  #after :finishing, 'deploy:cleanup' #task実行タイミングを指定できます。詳細は下記
+  #http://capistranorb.com/documentation/getting-started/flow/
+    #サンプルにunicorn再起動タスク
+    # desc 'Restart application'
+    task :restart do
+      invoke 'unicorn:restart' #lib/capustrano/tasks/unicorn.cap内処理を実行
+    end
+  # after :finishing, 'deploy:cleanup'
 end
+
+
+# config valid for current version and patch releases of Capistrano
+
+# コメントアウトにlockから.gitまでを変えた。
+# lock "~> 3.11.0"
+
+# set :application, "my_app_name"
+# set :repo_url, "git@example.com:me/my_repo.git"
 
 # Default branch is :master
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
